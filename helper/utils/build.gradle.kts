dea1b25eb32e588a6bfc8662702c32ff5e478edc
Inyong Tisto <imtisto@gmail.com>
2026-04-04 14:48:56 +0700

    feat: refactor preview annotations and update build.gradle.kts for improved organization and clarity

--- helper/utils/build.gradle.kts
+++ helper/utils/build.gradle.kts
@@ -38,11 +38,6 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.compose.runtime)
-            implementation(libs.compose.foundation)
-            implementation(libs.compose.material3)
-            implementation(libs.compose.ui)
-            implementation(libs.compose.components.resources)
-            implementation(libs.compose.uiToolingPreview)
             implementation(libs.lifecycle.viewmodelCompose)
             implementation(libs.lifecycle.runtimeCompose)
         }
