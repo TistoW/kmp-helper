2888dcd803fd897c45c33998a2a1d7d513b02391
Inyong Tisto <imtisto@gmail.com>
2026-04-07 11:22:08 +0700

    feat: add expect/actual implementation for getStringExpect function

---
+++ helper/utils/src/androidMain/kotlin/com/tisto/kmp/helper/utils/expect/UtilsExpect.android.kt
@@ -0,0 +1,5 @@
+package com.tisto.kmp.helper.utils.expect
+
+actual fun getStringExpect(): String {
+    TODO("Not yet implemented")
+}
---
+++ helper/utils/src/commonMain/kotlin/com/tisto/kmp/helper/utils/expect/UtilsExpect.kt
@@ -0,0 +1,3 @@
+package com.tisto.kmp.helper.utils.expect
+
+expect fun getStringExpect(): String
