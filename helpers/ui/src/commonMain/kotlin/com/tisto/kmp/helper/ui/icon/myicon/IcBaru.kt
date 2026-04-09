2addd8f9881b0177699bfa86cf3c49c60c90003a
Inyong Tisto <imtisto@gmail.com>
2026-04-10 00:06:14 +0700

    feat: add Kotlin Multiplatform support and new icon components

--- helpers/ui/src/commonMain/kotlin/com/tisto/kmp/helper/ui/icon/myicon/IcBaru.kt
+++ helpers/ui/src/commonMain/kotlin/com/tisto/kmp/helper/ui/icon/myicon/IcBaru.kt
@@ -1,4 +1,5 @@
 package com.tisto.kmp.helper.ui.icon.myicon
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
@@ -27,53 +28,70 @@ val MyIcon.IcClose: ImageVector
         ).apply {
 
             path(
-                fill = SolidColor(Color(0.204f, 0.251f, 0.490f)),
+                fill = SolidColor(Color(0xFF34407D)),
                 fillAlpha = 1f,
                 pathFillType = PathFillType.NonZero
             ) {
                 moveTo(1744f, 124f)
                 curveToRelative(-387f, 404f, -775f, 808f, -1162f, 1212f)
+                curveToRelative(-373f, 390f, -360f, 1014f, 30f, 1388f)
+                curveToRelative(404f, 387f, 808f, 774f, 1212f, 1161f)
+                curveToRelative(122f, 117f, 316f, 112f, 432f, -9f)
+                curveToRelative(387f, -404f, 775f, -808f, 1162f, -1212f)
+                curveToRelative(373f, -390f, 360f, -1014f, -30f, -1388f)
+                curveToRelative(-404f, -387f, -808f, -774f, -1212f, -1161f)
+                curveToRelative(-122f, -117f, -316f, -112f, -432f, 9f)
                 close()
                 moveTo(1806f, 3185f)
                 curveToRelative(-5f, 54f, 14f, 109f, 55f, 150f)
-                lineToRelative(0f, 0f)
                 curveToRelative(22f, 20f, 46f, 35f, 73f, 44f)
-                lineToRelative(0f, 0f)
+                curveToRelative(18f, 6f, 38f, 9f, 58f, 9f)
                 curveToRelative(58f, 1f, 110f, -26f, 145f, -67f)
-                lineToRelative(0f, 0f)
+                curveToRelative(273f, -280f, 737f, -757f, 1005f, -1033f)
+                curveToRelative(144f, -148f, 141f, -387f, -7f, -531f)
+                curveToRelative(-212f, 218f, -494f, 508f, -764f, 785f)
                 curveToRelative(-41f, 43f, -109f, 45f, -153f, 4f)
+                curveToRelative(-18f, -17f, -30f, -39f, -33f, -63f)
                 lineToRelative(0f, -46f)
                 curveToRelative(3f, -440f, 8f, -1282f, 11f, -1636f)
-                lineToRelative(0f, 0f)
+                curveToRelative(0f, -104f, -84f, -189f, -187f, -189f)
                 curveToRelative(-31f, -1f, -61f, 7f, -87f, 20f)
-                lineToRelative(0f, 0f)
+                curveToRelative(-17f, 9f, -34f, 21f, -49f, 37f)
+                curveToRelative(-271f, 278f, -744f, 764f, -1015f, 1043f)
+                curveToRelative(-144f, 148f, -141f, 387f, 7f, 531f)
+                curveToRelative(212f, -218f, 494f, -508f, 764f, -785f)
                 curveToRelative(41f, -43f, 109f, -45f, 153f, -5f)
-                lineToRelative(0f, 0f)
+                curveToRelative(18f, 18f, 30f, 40f, 33f, 64f)
                 curveToRelative(-3f, 556f, -7f, 1112f, -9f, 1668f)
                 close()
             }
 
 
             path(
-                fill = SolidColor(Color(0.996f, 0.996f, 0.996f)),
+                fill = SolidColor(Color(0xFFFEFEFE)),
                 fillAlpha = 1f,
                 pathFillType = PathFillType.NonZero
             ) {
                 moveTo(1806f, 3184f)
                 curveToRelative(-5f, 55f, 14f, 110f, 55f, 151f)
-                lineToRelative(0f, 0f)
                 curveToRelative(22f, 20f, 46f, 35f, 73f, 44f)
-                lineToRelative(0f, 0f)
+                curveToRelative(18f, 6f, 38f, 9f, 58f, 9f)
                 curveToRelative(58f, 1f, 110f, -26f, 145f, -67f)
-                lineToRelative(0f, 0f)
+                curveToRelative(273f, -280f, 737f, -758f, 1005f, -1033f)
+                curveToRelative(144f, -148f, 141f, -387f, -7f, -531f)
+                curveToRelative(-212f, 218f, -494f, 508f, -764f, 785f)
                 curveToRelative(-41f, 43f, -109f, 45f, -153f, 4f)
+                curveToRelative(-18f, -17f, -30f, -39f, -33f, -63f)
                 lineToRelative(0f, -46f)
                 curveToRelative(3f, -440f, 8f, -1282f, 11f, -1636f)
-                lineToRelative(0f, 0f)
+                curveToRelative(0f, -104f, -84f, -189f, -187f, -189f)
                 curveToRelative(-31f, -1f, -61f, 7f, -87f, 20f)
-                lineToRelative(0f, 0f)
+                curveToRelative(-17f, 9f, -34f, 21f, -49f, 37f)
+                curveToRelative(-271f, 278f, -744f, 764f, -1015f, 1043f)
+                curveToRelative(-144f, 148f, -141f, 387f, 7f, 531f)
+                curveToRelative(212f, -218f, 494f, -508f, 764f, -785f)
                 curveToRelative(41f, -43f, 109f, -45f, 153f, -5f)
-                lineToRelative(0f, 0f)
+                curveToRelative(18f, 18f, 30f, 40f, 33f, 64f)
                 curveToRelative(-3f, 556f, -7f, 1112f, -9f, 1667f)
                 close()
             }
