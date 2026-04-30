fed2e0217ddbc3eab7a3a75ec465b44cc348a51e
Inyong Tisto <imtisto@gmail.com>
2026-04-30 21:15:53 +0700

    fix: update kmp.targets.enabled property to true and change root project name to kiosk-kmp

--- helpers/ui/build.gradle.kts
+++ helpers/ui/build.gradle.kts
@@ -21,7 +21,7 @@ val localProperties = Properties().apply {
     }
 }
 
-val kmpTargetsEnabled = localProperties.getProperty("kmp.targets.enabled", "false").toBoolean()
+val kmpTargetsEnabled = localProperties.getProperty("kmp.targets.enabled", "true").toBoolean()
 
 
 kotlin {
--- helpers/utils/build.gradle.kts
+++ helpers/utils/build.gradle.kts
@@ -22,7 +22,7 @@ val localProperties = Properties().apply {
     }
 }
 
-val kmpTargetsEnabled = localProperties.getProperty("kmp.targets.enabled", "false").toBoolean()
+val kmpTargetsEnabled = localProperties.getProperty("kmp.targets.enabled", "true").toBoolean()
 
 kotlin {
     androidTarget {
